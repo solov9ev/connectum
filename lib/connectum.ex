9e4aa72ced594752e91106f0917cdf28362994a9
solov9ev <a.solov9ev@yandex.ru>
2019-03-22 13:36:40 +0300

    Create Connectum.order_expand_information/1 funtion, which controls the correctness of the parameters

--- lib/connectum.ex
+++ lib/connectum.ex
@@ -1,5 +1,30 @@
 defmodule Connectum do
   @moduledoc """
-  API library for payment services Connectum
+  Library for payment services Connectum
   """
+
+  @order_expand [
+    "card",
+    "client",
+    "location",
+    "custom_fields",
+    "issuer",
+    "secure3d",
+    "operations.cashflow"
+  ]
+
+  def order_expand_information(expand) when is_list(expand) do
+    case expand do
+      [] ->
+        {:ok, ""}
+
+      expand ->
+        # credo:disable-for-next-line Credo.Check.Refactor.Nesting
+        if Enum.all?(expand, fn item -> Enum.member?(@order_expand, item) end) do
+          {:ok, "expand=" <> Enum.join(expand, ",")}
+        else
+          {:error, %{reason: "Invalid expand"}}
+        end
+    end
+  end
 end
